aadadaa87187f12611193b3c42c12c8494ca967a
Chandler <chandlerd8@gmail.com>
2018-04-24 10:13:53 -0400

    minor fixes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,46 +1,58 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Fa, Container, Mask, View } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './Nav.css';
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarNav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  NavLink,
+  Fa,
+  Container,
+  Mask,
+  View
+} from "mdbreact";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./Nav.css";
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        collapse: false,
-        isWideEnough: false,
-      };
-  this.onClick = this.onClick.bind(this);
-}
+    this.state = {
+      collapse: false,
+      isWideEnough: false
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-onClick(){
-  this.setState({
-    collapse: !this.state.collapse,
-  });
-}
-render() {
-  return (
-            <Router>
-              <Navbar dark expand="md" fixed="top" scrolling>
-                <NavbarBrand href="/">
-                    <strong>SHAREMAGIC</strong>
-                </NavbarBrand>
-                { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
-                <Collapse isOpen = { this.state.collapse } navbar>
-                  <NavbarNav center>
-                    <NavItem active>
-                        <NavLink to="/">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/Login">Login</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/Bookings">Bookings</NavLink>
-                    </NavItem>
-                  </NavbarNav>
-                </Collapse>
-              </Navbar>
-            </Router>
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <Navbar dark expand="md" fixed="top" scrolling>
+          <NavbarBrand href="/">
+            <strong>SHAREMAGIC</strong>
+          </NavbarBrand>
+          {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+          <Collapse isOpen={this.state.collapse} navbar>
+            <NavbarNav center>
+              <NavItem active>
+                <NavLink to="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/login">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/Bookings">Bookings</NavLink>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+        </Navbar>
+      </Router>
     );
   }
 }
